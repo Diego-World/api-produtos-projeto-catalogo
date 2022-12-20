989fcf523003b530cc3e5d3ecfc0db6617d6a758
Diego Gouveia <83849414+Diego-World@users.noreply.github.com>
2022-12-20 18:18:24 -0300

    Get, GetById,Post: Done!

--- src/main/java/com/diegogouveia/springreactcatalog/controller/ProdutoController.java
+++ src/main/java/com/diegogouveia/springreactcatalog/controller/ProdutoController.java
@@ -3,7 +3,6 @@ package com.diegogouveia.springreactcatalog.controller;
 import com.diegogouveia.springreactcatalog.datasourcemodel.Produto;
 import com.diegogouveia.springreactcatalog.repositories.ProdutoRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,13 +12,18 @@ import java.util.UUID;
 @RestController
 @RequestMapping(value = "api/")
 public class ProdutoController {
-
     @Autowired
     private ProdutoRepositories produtoRepositories;
 
-    @GetMapping(path = "produto")
-    public List<Produto> buscarProdutoPorId(@PathVariable(name = "id", required = true)UUID id){
+    // Buscar todos os produtos
+    @GetMapping(path = "/produtos/buscar")
+    public List<Produto> buscarTodosProduto(){
         return produtoRepositories.findAll();
+    }
+
+    @GetMapping(path = "/produtos/buscar/id/{id}")
+    public Optional<Produto> buscarProdutoPeloId(@PathVariable(name = "id", required = true)UUID id){
+        return produtoRepositories.findById(id);
     }
 
     @PostMapping(path = "produtos/save")
